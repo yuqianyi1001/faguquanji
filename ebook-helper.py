d2b4083c552d9df8bf383f99542cc06835eedc9f
Jeff <52897651+yuqianyi1001@users.noreply.github.com>
2023-12-08 19:27:45 -0800

    adding PDF files

--- ebook-helper.py
+++ ebook-helper.py
@@ -5,10 +5,16 @@ import subprocess, os
 
 # return name of file to be kept after conversion.
 # we are just changing the extension. mobi here.
-def get_final_filename(f):
+def get_final_filename_mobi(f):
     f = f.split(".")
     filename = ".".join(f[0:-1])
     processed_file_name = filename+".mobi"
+    return processed_file_name
+
+def get_final_filename_pdf(f):
+    f = f.split(".")
+    filename = ".".join(f[0:-1])
+    processed_file_name = filename+".pdf"
     return processed_file_name
 
 # return file extension. pdf or epub or mobi
@@ -22,8 +28,15 @@ for root, dirs, files in walk('.'):
     for f in files:
         if f.lower().endswith('.epub'):
             src = os.path.abspath(root + '/' + f)
-            dest = get_final_filename(src)
+            dest_mobi = get_final_filename_mobi(src)
 
-            if not os.path.exists(dest):
-                subprocess.call([bin_file, src, dest]) 
-                print('converted ' + dest)
+            if not os.path.exists(dest_mobi):
+                subprocess.call([bin_file, src, dest_mobi]) 
+                print('converted ' + dest_mobi)
+
+            dest_pdf = get_final_filename_pdf(src)
+
+            if not os.path.exists(dest_pdf):
+                subprocess.call([bin_file, src, dest_pdf]) 
+                print('converted ' + dest_pdf)
+
